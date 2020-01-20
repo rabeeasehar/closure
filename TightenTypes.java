1ff20b04b56b2f86fae398a789e0977535a19e27
rabeeasehar <sehar198@gmail.com>
2020-01-21 00:39:45 +0100

    Update TightenTypes.java

--- TightenTypes.java
+++ TightenTypes.java
@@ -1,13 +1,1 @@
-       // Look for a function type in the argument list.
-        Iterator<Node> paramIter =
-            ((FunctionType) recvType).getParameters().iterator();
-        Iterator<Node> argumentIter = n.children().iterator();
-        argumentIter.next(); // Skip the function name.
-        while (paramIter.hasNext() && argumentIter.hasNext()) {
-          Node arg = argumentIter.next();
-          Node param = paramIter.next();
-          if (arg.getJSType() instanceof FunctionType) {
-            actions.addAll(getImplicitActionsFromArgument(
-                arg,
-                ((FunctionType) arg.getJSType()).getTypeOfThis(),
-                param.getJSType()));
+dfgdfg
